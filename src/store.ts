5b415d83dadfb7d2c951e2798a322fa9235ddc48
Andrey Shtepura <shtepura2605@outlook.com>
2025-05-22 00:21:03 +0300

    update TrackScreen and store to comment out calculator-related code

--- src/store.ts
+++ src/store.ts
@@ -1,17 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
 import { novaPostApi } from './services/api/api';
-import { calculatorApi } from './services/api/calculatorService';
+// import { calculatorApi } from './services/api/calculatorService';
 
 export const store = configureStore({
   reducer: {
     [novaPostApi.reducerPath]: novaPostApi.reducer,
-    [calculatorApi.reducerPath]: calculatorApi.reducer,
+    // [calculatorApi.reducerPath]: calculatorApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware()
       .concat(novaPostApi.middleware)
-      .concat(calculatorApi.middleware), 
+      // .concat(calculatorApi.middleware), 
 });
 
 setupListeners(store.dispatch);
